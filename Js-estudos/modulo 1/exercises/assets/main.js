bd1aba5f01d312cf33d71fa335c57ee4d0a8dd82
Alvaro.Veiga <alvarojoao17@gmail.com>
2024-04-10 19:12:53 -0300

    finish studing day

--- Js-estudos/modulo 1/exercises/assets/main.js
+++ Js-estudos/modulo 1/exercises/assets/main.js
@@ -12,7 +12,12 @@ let city = {
     nome: 'sao paulo',
     numero_de_habitantes: 100.000,
 }
+let dev = {
+    nome: 'alvaro',
+    qi: 81,
+}
 
 console.log(car)
 console.log(cake)
 console.log(city)
+console.log(dev)
--- Js-estudos/modulo 1/extra/src/assets/scripts.js
+++ Js-estudos/modulo 1/extra/src/assets/scripts.js
@@ -17,3 +17,14 @@ let idade = 90;
 let idadeString = `minha idade é ${idade}`;
 
 console.log(idadeString);
+
+//multiplas condicionais
+
+if (idade >= 18) {
+
+    if (idade >= 60) {
+        console.log('voce é idoso');
+    } else {
+    console.log('voce é adulto');
+    }
+}
