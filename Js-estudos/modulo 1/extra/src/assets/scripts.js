d8a99e34bcecf43d086667a94cc56e1e370fffc8
Alvaro Veiga <alvarojoao17@gmail.com>
2024-04-11 08:15:45 -0300

    studing

--- Js-estudos/modulo 1/extra/src/assets/scripts.js
+++ Js-estudos/modulo 1/extra/src/assets/scripts.js
@@ -20,11 +20,68 @@ console.log(idadeString);
 
 //multiplas condicionais
 
-if (idade >= 18) {
+// if (idade >= 18) {
 
-    if (idade >= 60) {
-        console.log('voce é idoso');
-    } else {
+//     if (idade >= 60) {
+//         console.log('voce é idoso');
+//     } else {
+//     console.log('voce é adulto');
+//     }
+// }
+
+if (idade >= 18 && idade < 60) {
     console.log('voce é adulto');
-    }
+} else if (idade >= 60) {
+    console.log('voce é idoso');
+} else {
+    console.log('voce é menor de idade');
+}
+
+
+// desmistificando o if
+
+let validação = true
+
+if (validação) {
+    console.log('verdadeiro');
+} else {
+    console.log('falso');
+}
+
+// subistitua o @ pela condcional correta para que
+// a mensagem seja impressa como TRUE
+
+let x = 10;
+let y = 5;
+
+console.log(x > y);
+
+//substitua o @ pela condicional correta para que
+//a mensagem seja impressa como FALSE
+
+let w = '10';
+let z = 10;
+
+console.log(w == z);
+
+
+//subistitua o @ pela condicional correta para que
+//a mensagem seja impressa como TRUE
+
+
+let a = 10;
+let b = 5;
+console.log(a != b);
+
+
+//crie uma condicional que verifica se o preço da fruta esta barato ou caro.
+//se o preço for menor que 10 imprima 'carne barata'
+
+
+let precoFruta = 9;
+
+if (precoFruta < 10) {
+    console.log('fruta barata');
+}else {
+    console.log('fruta cara');
 }
