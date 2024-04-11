cdd508d43453a8007e0763057d8f37359c5c4e45
Alvaro Veiga <alvarojoao17@gmail.com>
2024-04-11 07:03:12 -0300

    fix

--- Js-estudos/modulo 1/extra/src/assets/scripts.js
+++ Js-estudos/modulo 1/extra/src/assets/scripts.js
@@ -17,3 +17,21 @@ let idade = 90;
 let idadeString = `minha idade é ${idade}`;
 
 console.log(idadeString);
+
+
+// condicionais
+
+if (idade > 18) {
+    console.log('maior de idade');
+} else {
+    console.log('menor de idade');
+}
+
+//operadores lógicos
+
+if (idade == 20) {
+    console.log('sua idade é 20 anos');
+} else if ( idade == 30) {
+    idade = idade + 10;
+    console.log('sua idade é de 40 anos');
+}
