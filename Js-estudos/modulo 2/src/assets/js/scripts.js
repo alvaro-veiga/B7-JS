dcb7e291e2560ec4b6ada849fb9517a9daeb28c6
Alvaro Veiga <alvarojoao17@gmail.com>
2024-04-21 13:11:51 -0300

    save work

--- Js-estudos/modulo 2/src/assets/js/scripts.js
+++ Js-estudos/modulo 2/src/assets/js/scripts.js
@@ -1,16 +1,29 @@
 // functions
-
-// function to open and close the menu
-function openCloseMenu() {
-  const menu = document.querySelector('.menu');
-  const menuButton = document.querySelector('.menu-button');
-  const menuClose = document.querySelector('.menu-close');
-
-  menuButton.addEventListener('click', () => {
-    menu.classList.add('active');
-  });
-
-  menuClose.addEventListener('click', () => {
-    menu.classList.remove('active');
-  });
+function gravidade() {
+  console.log('A gravidade do planeta é:');
+  console.log(9.8);
 }
+
+gravidade();
+
+// functons with parameters
+
+function velocidade(espaco, tempo) {
+  console.log('A velocidade é:');
+  console.log(espaco / tempo);
+}
+
+velocidade(100, 20);
+
+
+// functions with return
+
+function somar(a, b) {
+  return a + b;
+}
+let resultado = somar(10, 5);
+
+console.log(resultado);
+
+
+// entrada -> processamento -> saída
