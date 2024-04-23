cd95c668ddcdd7ee3640fa5bc04032a339ef65c0
Alvaro Veiga <alvarojoao17@gmail.com>
2024-04-23 18:21:20 -0300

    continue studding

---
+++ Js-estudos/modulo 2/arrow/assets/js/main.js
@@ -0,0 +1,15 @@
+//function sum(x, y) {
+//  return x + y;
+//}
+//console.log(sum(1, 2));
+
+//exemple arrow function
+const sum = (x, y) => {
+    let result = x + y;
+    return result;
+}
+
+const multiply = (x, y) => x * y;
+
+console.log(sum(1, 2));
+console.log(multiply(2, 3));
